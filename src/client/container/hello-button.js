a43a3a3bbd3a6c8fc05199dc45b27106a9129b05
lucky <lucky_soft@163.com>
2019-01-06 13:10:56 +0800

    add react-redux support

---
+++ src/client/container/hello-button.js
@@ -0,0 +1,17 @@
+// @flow
+
+import {connect} from 'react-redux'
+
+import {sayHello} from '../action/hello'
+
+import Button from '../component/button'
+
+const mapStateToProps = () => ({
+  label: 'say hello'
+})
+
+const mapDispatchToProps = dispatch => ({
+  handleClick: () => {dispatch(sayHello('hello'))}
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Button)
